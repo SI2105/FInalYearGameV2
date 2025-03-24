5ca5ab53309855fb3d7c3d328f19e64eed61beab
ec22612 <120340281+SI2105@users.noreply.github.com>
2025-03-24 23:36:15 +0000

    Main Character Implemented - Main Character Sprite imported - Player animations created and blended - Player controller script created - Player controls implemented with new input system

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+   
+    SpriteRenderer sprite;
+    [Header("Movement")]
+    public float movespeed = 5.0f;
+    float horizontalmovement;
+    [Header("Jump")]
+    public float jumpforce = 1.0f;
+
+    [Header("GroundCheck")]
+    public Transform groundCheck;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
+
+    [Header("Gravity")]
+    public float gravity = 1.0f;
+    public float maxfallspeed = 10f;
+    public float fallMultiplier = 2.5f;
+
+    private Animator _animator;
+    private const string _horizontal = "Horizontal";
+    private const string _vertical = "Vertical";
+    private const string _lastHorizontal = "LastHorizontal";
+    private const string _lastVertical = "LastVertical";
+ 
+
+    void Start()
+    {
+        
+      
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+
+
+
+    }
+
+
+    void FixedUpdate()
+    {
+        
+        rigidbody2d.velocity = new Vector2(horizontalmovement * movespeed, rigidbody2d.velocity.y);
+        _animator.SetFloat("xVelocity", Mathf.Abs(rigidbody2d.velocity.x));
+        _animator.SetFloat("yVelocity", rigidbody2d.velocity.x);
+        if (isGrounded())
+        {
+            _animator.SetBool("IsJumping", false);
+        }
+        //Gravity();
+  
+
+
+
+
+    }
+
+    private void OnDrawGizmosSelected() 
+    {
+        //Dev Aid
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheck.position, groundCheckSize);
+
+    }
+
+
+ 
+    public void Move(InputAction.CallbackContext context) {
+
+        //Movement Input Action
+        horizontalmovement = context.ReadValue<Vector2>().x;
+
+        //Handle Sprite direction
+        if (horizontalmovement > 0)
+        {
+            sprite.flipX = true;
+        }
+
+        else if (horizontalmovement < 0) { 
+            sprite.flipX = false;
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context) {
+        if (isGrounded()){
+            if (context.performed)
+            {
+                rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, jumpforce);
+                _animator.SetBool("IsJumping", true);
+            }
+            else if (context.canceled){
+                rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, rigidbody2d.velocity.y * jumpforce/2);
+           
+
+            }
+        }
+    }
+
+    private bool isGrounded() {
+        if (Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0, groundLayer)) {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void Gravity() {
+        //Deprecated Gravity Code
+        if (rigidbody2d.velocity.y < 0)
+        {
+            rigidbody2d.gravityScale = gravity * fallMultiplier;
+            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, -maxfallspeed);
+        }
+        else {
+            rigidbody2d.gravityScale = gravity;
+        }
+    }
+    
+}
